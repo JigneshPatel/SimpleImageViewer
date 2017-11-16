ea63a84e531ce293a8e8243877ad068be8303c54
Andrea Mazzini <andrea.mazzini@gmail.com>
2017-11-16 16:51:46 +0100

    Add delete button

--- ImageViewer/ImageViewerConfiguration.swift
+++ ImageViewer/ImageViewerConfiguration.swift
@@ -5,13 +5,17 @@ public typealias ImageCompletion = (UIImage?) -> Void
 public typealias ImageBlock = (@escaping ImageCompletion) -> Void
 
 public final class ImageViewerConfiguration {
-    public var image: UIImage?
-    public var imageView: UIImageView?
-    public var imageBlock: ImageBlock?
-    
-    public typealias ConfigurationClosure = (ImageViewerConfiguration) -> ()
-    
-    public init(configurationClosure: ConfigurationClosure) {
-        configurationClosure(self)
-    }
+  public var image: UIImage?
+  public var imageView: UIImageView?
+  public var imageBlock: ImageBlock?
+  public var deleteImage: UIImage?
+  public var deleteText: String?
+  public var deleteAttributes: [NSAttributedStringKey: Any]?
+  public var deleteHandler: (() -> Void)?
+  
+  public typealias ConfigurationClosure = (ImageViewerConfiguration) -> ()
+  
+  public init(configurationClosure: ConfigurationClosure) {
+    configurationClosure(self)
+  }
 }
